e16e6336f53561faeb010969466fe6a39e6d22c5
Maryna Kucher <maryna.dev.k@gmail.com>
2024-06-19 14:03:51 +0300

    Movie detail and search routes are implemented

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -19,5 +19,36 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.get('/movie/:id',(req, res, next)=>{
+  const movieId = req.params.id;
+  const thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`
+  console.log(thisMovieUrl);
+  request.get(thisMovieUrl,(error, response, movieData)=>{
+    console.log(typeof(movieData))
+    const parsedData = JSON.parse(movieData)
+    res.render('single-movie',{
+      parsedData
+    })
+  })
+});
+
+  router.post('/search',(req, res, next)=>{
+    const userSearchTerm = encodeURI(req.body.movieSearch);
+    const cat = req.body.cat;
+    const movieUrl = `${apiBaseUrl}/search/${cat}?query=${userSearchTerm}&api_key=${apiKey}`
+
+    request.get(movieUrl,(error, response, movieData)=>{
+      let parsedData = JSON.parse(movieData);
+
+      if (cat === "person") {
+        parsedData.results = parsedData.results[0].known_for;
+      }
+
+      res.render('index', {
+        parsedData: parsedData.results
+      })
+    })
+  })
+
 
 module.exports = router;
