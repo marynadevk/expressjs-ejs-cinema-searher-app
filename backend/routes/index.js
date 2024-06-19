8fee868a63c797549ac42263540e8313cb891e38
Maryna Kucher <maryna.dev.k@gmail.com>
2024-06-19 17:49:05 +0300

    Templates for index and movie routs are created, data for making reqests is added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const movies = require('../data/movies');
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/most_popular', (req, res, next) => {
+  let page = req.query.page;
+  if (page === undefined) {
+    page = 1;
+  }
+  let results = movies.filter(movie => movie.most_popular);
+  const indexToStart = (page - 1) * 20;
+
+  results = results.slice(indexToStart, indexToStart + 19);
+  res.json({ page, results });
+})
 
 module.exports = router;
