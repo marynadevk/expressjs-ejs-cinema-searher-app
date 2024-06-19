4ad226517433c6738c08b424f5d9e9b80871a686
Maryna Kucher <maryna.dev.k@gmail.com>
2024-06-19 13:57:28 +0300

    Initial commit for project

---
+++ frontend/routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');
+
+const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
+router.use((req, res, next) => {
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next();
+});
+
+router.get('/', function(req, res, next) {
+  request.get(nowPlayingUrl, (error, response, movieData) => {
+    const parsedData = JSON.parse(movieData);
+    res.render('index', { parsedData: parsedData.results });
+  })
+});
+
+
+module.exports = router;
